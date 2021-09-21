ff086337e5dc7a62b2adc7c41c186442b617a650
misaka9981 <932353913@qq.com>
2021-09-21 19:59:51 +0800

    sort

---
+++ src/Ch2/Ch2_1/Selection.java
@@ -0,0 +1,38 @@
+package Ch2.Ch2_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Selection {
+    public static void sort(Comparable[] a){
+        int N =a.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i+1; j < N; j++) {
+                if(less(a[j],a[min])) min = j;
+            }
+        }
+    }
+    private static boolean less(Comparable v,Comparable w){
+        return v.compareTo(w) < 0;
+    }
+
+    private static void show(Comparable[] a){
+        for (int i = 0; i < a.length; i++)
+            StdOut.print(a[i] + " ");
+        StdOut.println();
+    }
+
+    private static void exch(Comparable[] a,int i,int j){
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        // 测试数组元素是否有序
+        for(int i = 1; i < a.length; i++)
+            if(less(a[i], a[i - 1]))
+                return false;
+        return true;
+    }
+}
